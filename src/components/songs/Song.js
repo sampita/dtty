6a70666d2a59e0efa1b613e4244aa290017d5ffa
Sam Pita <samanthakpita@gmail.com>
2019-12-17 14:25:26 -0600

    implemented Firebase Cloud Storage

--- src/components/songs/Song.js
+++ src/components/songs/Song.js
@@ -3,7 +3,7 @@ import ApiManager from "../modules/ApiManager";
 import SongDetails from "./SongDetails";
 import SongDetailsEdit from "./SongDetailsEdit";
 import TextareaAutosize from 'react-autosize-textarea';
-import * as firebase from 'firebase/app'; 
+import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import "./Song.css";
 import "./Footer.css";
@@ -20,33 +20,51 @@ class SongView extends Component {
 
     handleFieldChange = (evt) => {
         this.setState({
-                [evt.target.name]: evt.target.value
-            })
+            [evt.target.name]: evt.target.value
+        })
     }
 
     audioFileHandler = (evt) => {
-        // step 1: save Audio to Firebase
-        const recorder = document.getElementById('recorder');
-        const player = document.getElementById('player');
+        // const player = document.getElementById('player');
+        // const recorder = document.getElementById('recorder');
         const file = evt.target.files[0];
-        const url = URL.createObjectURL(file)
-
-        // var storageRef = firebase.storage().ref();
-        // var audioRef = storageRef.child('audio');
-
+        const songId = this.props.match.params.songId
         const audioRef = firebase.storage().ref('audio');
         const childRef = audioRef.child(`song-${this.state.songId}-user-${this.state.userId}`);
 
-        childRef.put(this.state.audio)
+    // step 1: save audio to Firebase
+        childRef.put(file)
+    // step 2: get url from firebase
+        .then(response => response.ref.getDownloadURL())
+    // step 3: save everything to json server
+        .then(downloadUrl => {
+                const newAudioURL = {
+                    audio: downloadUrl
+                }
+                return ApiManager.patch("songs", songId, newAudioURL)
+        })
+    // step 4: update state with new audio file url
+        .then((patchedSong) => {
+            console.log("patchedSong", patchedSong)
+            this.setState({
+                audio: patchedSong.audio
+            })
+        })
+    }
 
-        // step 2: get url from firebase
-
-
-        console.log("url", url)
-        player.src = url;
-
-
-
+    getUpdatedSongInfo() {
+    const songId = this.props.match.params.songId
+    const userId = localStorage.getItem("user")
+        ApiManager.get("songs", songId)
+            .then((song) => {
+                this.setState({
+                    title: song.title,
+                    lyrics: song.lyrics,
+                    audio: song.audio,
+                    songId: song.id,
+                    userId: userId
+                })
+            })
     }
 
     updateTitleandLyricsAndReturnToHome = evt => {
@@ -58,7 +76,7 @@ class SongView extends Component {
         };
 
         ApiManager.patch("songs", songId, updatedTitleAndLyrics)
-        .then(() => this.props.history.push("/"))
+            .then(() => this.props.history.push("/"))
 
     }
 
@@ -71,20 +89,7 @@ class SongView extends Component {
     }
 
     componentDidMount() {
-        const songId=this.props.match.params.songId
-        const userId=localStorage.getItem("user")
-        ApiManager.get("songs", songId)
-        .then((song) => {
-            this.setState({
-                title: song.title,
-                lyrics: song.lyrics,
-            })    
-        })
-
-        this.setState({
-            userId: userId,
-            songId: songId
-        })
+        this.getUpdatedSongInfo()
     }
 
     render() {
@@ -97,22 +102,32 @@ class SongView extends Component {
                         onChange={(evt) => this.handleFieldChange(evt)}
                         value={this.state.title}></input>
                 </header>
-                <section>
-                    <input type="file" accept="audio/*" capture id="recorder"onChange={(evt) => this.audioFileHandler(evt)}/>
-                    <audio id="player" controls></audio>
+                <section id="audioContainer">
+                    <input
+                        type="file"
+                        accept="audio/*"
+                        capture
+                        id="recorder"
+                        onChange={(evt) => this.audioFileHandler(evt)} />
+                    <audio
+                        id="player"
+                        controls
+                        src={this.state.audio}
+                    >
+                    </audio>
                 </section>
                 <div className="lyricsTextArea paper">
                     <div className="lines">
-                <TextareaAutosize
-                    type="text"
-                    name="lyrics"
-                    className="lyricsTextBox text"
-                    placeholder="Write your lyrics here..."
-                    onChange={(evt) => this.handleFieldChange(evt)}
-                    onResize={(e) => {}}
-                    value={this.state.lyrics}>
-                </TextareaAutosize>
-                </div>
+                        <TextareaAutosize
+                            type="text"
+                            name="lyrics"
+                            className="lyricsTextBox text"
+                            placeholder="Write your lyrics here..."
+                            onChange={(evt) => this.handleFieldChange(evt)}
+                            onResize={(e) => { }}
+                            value={this.state.lyrics}>
+                        </TextareaAutosize>
+                    </div>
                 </div>
                 <footer id="songFooter">
                     {this.state.editSongDetails ? (
