9967da509fc2cfe98ced0641a874b06e457c0ee5
Sam Pita <samanthakpita@gmail.com>
2020-01-06 11:39:11 -0600

    styled Song Card

--- src/components/songs/SongCard.js
+++ src/components/songs/SongCard.js
@@ -5,23 +5,26 @@ import "./SongCard.css";
 import ApiManager from '../modules/ApiManager';
 import moment from 'moment';
 
+
 class SongCard extends Component {
     state = {
         isSongSelected: false,
-        hidden: true,
         tags: []
     }
 
     handleSelectedSongChange = (event) => {
+        let toggleSelectedSong = this.state.isSongSelected
+        toggleSelectedSong = toggleSelectedSong ? false : true;
         this.setState({
-            isSongSelected: true
+            isSongSelected: toggleSelectedSong
         })
     }
 
+
     componentDidMount = () => {
         const songId = this.props.id
-        ApiManager.getItemsForSpecificSong("tags", songId).then(tagsArray => 
-            this.setState({tags: tagsArray}));
+        ApiManager.getItemsForSpecificSong("tags", songId).then(tagsArray =>
+            this.setState({ tags: tagsArray }));
     }
 
     render() {
@@ -31,24 +34,31 @@ class SongCard extends Component {
                     <div className="cardContent">
                         <section className="tagContainer">
                             {this.state.tags.map(tag =>
-                            <Label key={tag.id} color='purple' horizontal>
-                                {tag.tag}
-                            </Label>
+                                <Label key={tag.id} color='purple' horizontal>
+                                    {tag.tag}
+                                </Label>
                             )}
                         </section>
                         <h3>{this.props.song.title}</h3>
                         <p>Last updated {moment(this.props.song.lastUpdated).format("MMMM D, YYYY")}</p>
                         <p>{this.props.song.length}</p>
                     </div>
-                
-                {this.state.isSongSelected ? (
-                    <section className="cardButtonContainer">
-                        <FontAwesomeIcon icon="trash-alt" type="button" onClick={() => this.props.deleteSong(this.props.id)} />
-                        <FontAwesomeIcon icon="play" type="button" />
-                        <FontAwesomeIcon icon="ellipsis-h" type="button" onClick={() => {this.props.history.push(`/songs/${this.props.id}`)}} />
-                </section>
-                ) : null}
-                {/* <div className="ui fitted divider"></div> */}
+
+                    {this.state.isSongSelected ? (
+                        <div>
+                        <audio
+                            id="player"
+                            controls
+                            src={this.props.song.audio}
+                        >
+                            </audio>
+                        <section className="cardButtonContainer">
+                            <FontAwesomeIcon icon="trash-alt" type="button" onClick={() => this.props.deleteSong(this.props.id)} />
+                            <FontAwesomeIcon icon="play" type="button" onClick={() => document.getElementById('player').play()}/>
+                            <FontAwesomeIcon icon="ellipsis-h" type="button" onClick={() => { this.props.history.push(`/songs/${this.props.id}`) }} />
+                        </section>
+                        </div>
+                    ) : null}
                 </article>
             </>
         );
