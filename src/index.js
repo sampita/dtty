5d38c0ea5f9586cab7ffc236849106c1ee05b21d
maxcwolf <maxcharleswolf@gmail.com>
2020-02-07 17:27:27 -0600

    :spiral_notepad: serious README and script changes :fire:

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
-import './index.css';
-import {firebaseConfig} from "./config/FirebaseConfig"
-// import App from './App';
-import * as serviceWorker from './serviceWorker';
-// import withFirebaseAuth from 'react-with-firebase-auth';
-import * as firebase from "firebase/app";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { firebaseConfig } from './config/FirebaseConfig'
+import { App } from './App'
+import * as serviceWorker from './serviceWorker'
+import * as firebase from 'firebase/app'
 import 'firebase/analytics'
-import 'firebase/auth';
+import 'firebase/auth'
 import 'semantic-ui-css/semantic.min.css'
-import Dtty from './components/Dtty';
-
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
-ReactDOM.render(<Router>
-    <Dtty />
-</Router>
-    , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
