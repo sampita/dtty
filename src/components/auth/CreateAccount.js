f335466f5ac68a36d0d136cfc15a1c90cfae4a56
maxcwolf <maxcharleswolf@gmail.com>
2020-02-07 21:46:20 -0600

    :pencil: added a top level comment to CreateAccount component

--- src/components/auth/CreateAccount.js
+++ src/components/auth/CreateAccount.js
@@ -25,6 +25,18 @@ const reducer = (state, action) => {
   }
 }
 
+/**
+ *
+ * @name AccountCreate - this should be named something more like this. CreateAccount implies that it is a utility rather than a Component. We can discuss this later
+ * @description Account creation form. A stateful component which holds the user's registration data, validates it, and processes it using the ApiManager
+ *
+ * @notes
+ *  - It has been refactored to be a functional component using the `useReducer` hook for state management.
+ *  - Functions and conditionals have been broken up into reasonably sized smaller parts.
+ *  - Variables (except the Component name) have been renamed to try to explain exactly what they do, since now they should do one or two things.
+ *  - Promises have been refactored to use modern ES7 "async/await" syntax to clean up the `.then` callback chaining.
+ *  - We are programatically giving the `onFieldChange` function its variables, rather than inferring them from element attributes. JavaScript FTW!
+ */
 export const CreateAccount = ({ setUser, history }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
@@ -34,7 +46,7 @@ export const CreateAccount = ({ setUser, history }) => {
   const userEmailExists = users =>
     users && users.find(user => user.email.toLowerCase() === state.email.toLowerCase())
 
-  const userPasswordMismatch = password !== confirmPassword
+  const userPasswordsMatch = password === confirmPassword
   const allFieldsValid = email && password && confirmPassword
   const userEmailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
 
@@ -43,7 +55,7 @@ export const CreateAccount = ({ setUser, history }) => {
 
     if (userEmailExists(users)) alert('This email is already taken')
 
-    if (userPasswordMismatch) alert('Your passwords do not match')
+    if (!userPasswordsMatch) alert('Your passwords do not match')
 
     if (!allFieldsValid) alert('Please fill out all fields')
 
